d8cd6727da35e8fd3da8a4c036567ce441bdc7b7
primusmagestri <47402373+primusmagestri@users.noreply.github.com>
2019-08-01 17:10:41 +0300

    some more

--- reldiv.js
+++ reldiv.js
@@ -1,12 +1,15 @@
  $("#loadstuff").click(function() {
     var milliseconds = (new Date).getTime();
-var expires = milliseconds+10;
+var expires = Math.round(milliseconds/1000)+100;
+//debugger;
 var path = 'players/kvnuuipe-4FuizHuQ.js';
 var secret='KsQ5gv8wydgEf6PtVuV0xqTi';
 var thevalue=path+':'+expires+':'+secret;
 var hash = md5(thevalue);
 var url = 'https://content.jwplatform.com/'+path+'?exp='+expires+'&sig='+hash;
 //url='https://cdn.jwplayer.com/players/kvnuuipe-4FuizHuQ.js';
+console.log(url);
+console.log(expires);
 
 var viddiv = document.getElementById('vid'),
     script = document.createElement('script'); // Create the script
